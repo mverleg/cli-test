c0ffeec26b806636ca501234b29b2651111f8845
Mark <mverleg.noreply@gmail.com>
2023-04-02 17:09:27 +0200

    Method to collect and parse tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,33 @@
-use log::debug;
+use ::log::debug;
+
+use crate::parse::CliTest;
 use crate::scan::find_cli_tests;
 
 pub use self::cli::Args;
 
 mod cli;
 mod scan;
+mod parse;
 
 pub fn cli_test(args: &Args) -> Result<(), String> {
     assert!(!args.roots.is_empty());
-    let pths = match args.path.as_ref() {
+    let tests = collect_tests(args)?;
+    todo!()
+}
+
+fn collect_tests(args: &Args) -> Result<Vec<CliTest>, String> {
+    let paths = match args.path.as_ref() {
         Some(pth) => {
             debug!("requested cli-test for file {}, not scanning for more tests", pth.to_string_lossy());
             vec![pth.to_owned()]
         },
         None => find_cli_tests(&args.roots, args.max_depth, args.minimum_tests)?,
     };
-    todo!()
+    let mut tests = Vec::with_capacity(paths.len());
+    for path in paths {
+        tests.push(CliTest {
+            path,
+        })
+    }
+    Ok(tests)
 }
---
+++ src/parse.rs
@@ -0,0 +1,6 @@
+use ::std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct CliTest {
+    pub(crate) path: PathBuf,
+}
