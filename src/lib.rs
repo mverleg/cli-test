3fbd5a358269f0d7194237aa69f01ce320c295a1
Mark <mverleg.noreply@gmail.com>
2023-04-02 17:04:15 +0200

    Prevent duplicate tests, add better logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use log::debug;
 use crate::scan::find_cli_tests;
 
 pub use self::cli::Args;
@@ -8,7 +9,10 @@ mod scan;
 pub fn cli_test(args: &Args) -> Result<(), String> {
     assert!(!args.roots.is_empty());
     let pths = match args.path.as_ref() {
-        Some(pth) => vec![pth.to_owned()],
+        Some(pth) => {
+            debug!("requested cli-test for file {}, not scanning for more tests", pth.to_string_lossy());
+            vec![pth.to_owned()]
+        },
         None => find_cli_tests(&args.roots, args.max_depth, args.minimum_tests)?,
     };
     todo!()
--- src/scan.rs
+++ src/scan.rs
@@ -1,11 +1,17 @@
-
 use ::std::collections::HashSet;
 use ::std::path::PathBuf;
 
 use ::log::debug;
 use ::walkdir::WalkDir;
 
+use ::std::env;
+
 pub fn find_cli_tests(roots: &[PathBuf], max_depth: u32, minimum_tests: u32) -> Result<Vec<PathBuf>, String> {
+    debug!("searching cli-tests in [{}] (cwd={}), max depth {max_depth}, expecting at least {minimum_tests} tests",
+        env::current_dir().map(|cwd| cwd.to_string_lossy().into_owned()).unwrap_or("?".to_owned()),
+        roots.iter()
+            .map(|root| root.to_string_lossy())
+            .collect::<Vec<_>>().join(", "));
     let mut results = Vec::new();
     let mut seen = HashSet::new();
     for root in roots {
@@ -16,9 +22,9 @@ pub fn find_cli_tests(roots: &[PathBuf], max_depth: u32, minimum_tests: u32) -> 
         for entry_res in walker.into_iter() {
             let entry = entry_res.map_err(|err| format!("count not scan for test files, err: {err}"))?;
             if let Some(name) = entry.file_name().to_str() {
-                if ! seen.insert(entry.path().to_owned()) {
+                if !seen.insert(entry.path().to_owned()) {
                     debug!("skipping duplicate test {}", entry.path().to_string_lossy());
-                    continue
+                    continue;
                 }
                 if name.ends_with(".clts") && entry.path().is_file() {
                     results.push(entry.into_path())
@@ -28,7 +34,7 @@ pub fn find_cli_tests(roots: &[PathBuf], max_depth: u32, minimum_tests: u32) -> 
     }
     debug!("found {} tests", results.len());
     if (minimum_tests as usize) < results.len() {
-        return Err(format!("expected at least {} tests, got {}", minimum_tests, results.len()))
+        return Err(format!("expected at least {} tests, got {}", minimum_tests, results.len()));
     }
     Ok(results)
 }
